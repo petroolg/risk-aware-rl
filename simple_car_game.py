5a117831d2aff5d6df72e73cb9932c431e75ffb4
petrovao <olga.petrova@avast.com>
2018-07-02 20:25:45 +0200

    Implement imitation an supervised agent.

--- simple_car_game.py
+++ simple_car_game.py
@@ -1,14 +1,19 @@
 import time
 import numpy as np
 import pygame
-from matplotlib import pyplot as plt
+# import matplotlib
+# matplotlib.use('TkAgg')
+# from matplotlib import pyplot as plt
 from pygame import QUIT, K_LEFT, K_RIGHT, K_DOWN, K_UP
 from pygame.colordict import THECOLORS
+from datetime import datetime
+import os
+# os.environ["SDL_VIDEODRIVER"] = "dummy"
 
 # Based on Policy Gradients with variance Related Risk Criteria
 
-ROAD_W = 30
-ROAD_H = 100
+ROAD_W = 12
+ROAD_H = 60
 
 def clamp(n, smallest, largest):
     return max(smallest, min(n, largest))
@@ -24,61 +29,65 @@ class car:
 
     def coord(self, y):
         x0 = self.pos[0] - self.size[0]/2
-        y0 = self.pos[1] - y + ROAD_H/2
+        y0 = ROAD_H - (self.pos[1] - y + 10)
         return int(x0), int(y0)
 
     def draw(self, display, y):
         x, y = self.coord(y)
-        pygame.draw.rect(display, self.color, (x,y,self.size[0],self.size[1]))
+        pygame.draw.rect(display, self.color, (x*5,y*5,self.size[0]*5,self.size[1]*5))
 
-# function plotting running average of vector vec, n specifies width of window
-def plot_run_avg(vec, n, **kwargs):
-    p = []
-    vec = np.array(vec)
-    for i in range(len(vec)):
-        p.append(np.mean(vec[int(max(0, i - n/2)) : int(min(i+n/2, len(vec)-1))]))
-    plt.plot(p, **kwargs)
+# # function plotting running average of vector vec, n specifies width of window
+# def plot_run_avg(vec, n, **kwargs):
+#     p = []
+#     vec = np.array(vec)
+#     for i in range(len(vec)):
+#         p.append(np.mean(vec[int(max(0, i - n/2)) : int(min(i+n/2, len(vec)-1))]))
+#     plt.plot(p, **kwargs)
 
 class road:
     def __init__(self, width, length, line1_vel, line2_vel):
         self.pole = np.zeros((length, width))
         self.width = width
         self.length = length
-        self.l2c = int(self.width/4) + 1
-        self.l1c = int(3 * self.width / 4) + 1
+        self.l2c = int(self.width/4)+1
+        self.l1c = int(3 * self.width / 4)-1
         self.l2v = line2_vel
         self.l1v = line1_vel
         self.vel = np.linspace(self.l1v, self.l2v, self.l1c - self.l2c)
 
 
 class Road_game:
-    def __init__(self, var_bound=0.0):
+    def __init__(self):
 
-        self.road = road(ROAD_W, ROAD_H, 5, 8)
+        self.road = road(ROAD_W, ROAD_H, 6, 8)
         self.car_size = [ROAD_W/5, 2*ROAD_W/5]
 
-        self.actions = np.array([-1, 0, 1])
+        self.goal = 2000
 
-        self.goal = 270
-
-        self.state_lenth = self.road.pole.shape[0]*self.road.pole.shape[1]
-        self.theta = (np.random.sample((self.state_lenth, len(self.actions))) - 0.5)/np.sqrt(self.state_lenth)
-
-        self.G = 0.0  # estimation of total reward
-        self.Var = 0.0  # estimation of variance of reward
-        self.var_bound = var_bound  # threshold of variance
-        self.alpha_step = 0.005  # step of gradient ascent
-        self.beta_step = 0.005  # step of gradient ascent
-        self.lam = 0.7  # penalization, related to the approximation of COP solution, equations (9) and (10)
+        self.state_length = self.road.pole.shape[0] * self.road.pole.shape[1]
 
         pygame.init()
-        self.DISPLAY = pygame.display.set_mode((ROAD_W+150, ROAD_H), 0, 32)
+        self.DISPLAY = pygame.display.set_mode((ROAD_W*5, ROAD_H*5), 0, 32)
         pygame.display.set_caption('risk-aware-rl')
+
+        self.car = None
+        self.game_over = False
+        self.camera = None
+
+        self.actios = [[1,1],
+                       [1,0],
+                       [1,-1],
+                       [0,1],
+                       [0,0],
+                       [0,-1],
+                       [-1,1],
+                       [-1,0],
+                       [-1,-1]]
 
 
     @property
     def state(self):
-        return self.road.pole.reshape(self.state_lenth)
+        return np.hstack((self.road.pole.reshape(self.state_length), self.car.v))
 
     def collision(self):
         x, y = self.car.pos - [self.car_size[0]/2, 0]
@@ -87,7 +96,7 @@ class Road_game:
             x1, y1 = v.pos - [self.car_size[0] / 2, 0]
             a1, b1 = v.pos + [self.car_size[0] / 2, self.car_size[1]]
             if not (a < x1 or a1 < x or b < y1 or b1 < y):
-                print('collision')
+                # print('collision')
                 return True
         return False
 
@@ -97,137 +106,95 @@ class Road_game:
             return True
         return False
 
-        # Mean reward, variance and parameter update function, equations (13)
-    def update(self, R, zk, type='Var', update_theta=True):
-        self.G += self.alpha_step * (R - self.G)
-        self.Var += self.alpha_step * (R * R - self.G * self.G - self.Var)
-        print('Variance', self.Var)
-        print('ZK:', np.min(zk), np.max(zk))
-        if update_theta:
-            g_prime = 0.0 if (self.Var - self.var_bound) < 0.0 else 2.0 * (self.Var - self.var_bound)
-            if type == 'Var':
-                print('Update:', (R - self.lam * g_prime * (R * R - 2.0 * self.G)))
-                self.theta += self.beta_step * (R - self.lam * g_prime * (R * R - 2.0 * self.G)) * zk
-            if type == 'Sharpe':
-                self.theta += self.beta_step / np.sqrt(self.Var) * \
-                              (R - (self.G * R * R - 2.0 * self.G * self.G * R) / (2 * self.Var)) * zk
-
     def auto_move(self, zk):
         p_a = self.sample_action()
-        zk += self.log_like()
         rew = self.move(p_a)
         return rew, zk
 
     def process_keys(self):
         keys = pygame.key.get_pressed()
-        p_a = np.zeros(3)
+        d_v = np.zeros(2)  # direction and velocity
+
         if keys[K_RIGHT]:
-            p_a[2] = 1.0
+            d_v[0] = 1.0
         elif keys[K_LEFT]:
-            p_a[0] = 1.0
-        else:
-            p_a[1] = 1.0
-        rew = self.move(p_a)
-        return rew, 0.0
+            d_v[0] = -1.0
+        if keys[K_UP]:
+            d_v[1] = 1
+        elif keys[K_DOWN]:
+            d_v[1] = -1
+        return d_v
 
     # function plays one game, computes total reward and zk along trajectory
-    def play_one(self, T):
-        total_rew, zk = 0.0, 0.0
+    def play_one(self, seed=None):
+        total_rew = 0.0
+        self.init_game(seed=seed)
+
+        s_a_pairs = []
+
+        while not self.game_over:
+            # rew, zk = self.auto_move(zk)
+            st = self.state
+            d_v = self.process_keys()
+            s_a_pairs.append(np.hstack((st,d_v)))
+            rew = self.move(d_v)
+            total_rew += rew
+            print('\rVelocity: {}'.format(self.car.v), end='')
+            time.sleep(0.1)
+        if not self.collision():
+            np.save('trajectories_rand_big/traj_{}.npy'.format(int(datetime.now().timestamp())), s_a_pairs)
+        elif np.array(s_a_pairs).shape[0]>80:
+            np.save('trajectories_rand_big/traj_{}.npy'.format(int(datetime.now().timestamp())), s_a_pairs[:-10])
+        return total_rew
+
+    def replay(self, traj):
         self.init_game()
         i = 0
-        # plt.imshow(self.road.pole)
-        # plt.waitforbuttonpress
-        while i < T and not self.game_over:
-            rew, zk = self.auto_move(zk)
-            # rew, zk = self.process_keys()
-            total_rew += rew
-            # time.sleep(0.2)
+        for t in traj:
+            rew = self.move(t[-2:])
+            print('\rVelocity: {}'.format(self.car.v), end='')
+            time.sleep(0.15)
             i+=1
-            # print(self.car.pos, i)
-            # plt.imshow(self.road.pole)
-            # plt.waitforbuttonpress()
-        return total_rew, zk
 
-    # function returns probability of being in th 2nd state using softmax policy
-    def sample_action(self, temperature=20.0):
-        # http://incompleteideas.net/sutton/book/ebook/node17.html
-        lst = self.state.dot(self.theta)/temperature
-        # print(lst)
-        e_lst = np.exp(lst)
-        return e_lst / np.sum(e_lst)
-
-    # gradient of log-likelihood used for computing zk
-    def log_like(self, temperature=2.0):
-        lst = self.state.dot(self.theta) / temperature
-        e_lst = np.exp(lst)
-        d_lst = np.repeat([[(e_lst[1]+e_lst[2]), (e_lst[0]+e_lst[2]), (e_lst[0]+e_lst[1])]], self.state_lenth, axis=0)
-        d_lst = np.multiply(self.state[np.newaxis].T, d_lst/np.sum(e_lst))
-        
-        return d_lst
 
     # initialization function, chooses state randomly
-    def init_game(self):
+    def init_game(self, seed):
         self.game_over = False
         self.ov = []
 
-        pos = np.random.choice(np.arange(0,self.road.width,3))
-        pos = np.random.choice(np.arange(0,self.road.width,3))
-        for i in range(3):
-            self.ov.append(car([self.road.l2c, pos], self.car_size,  self.road.l2v))
-            pos += np.random.choice(np.arange(int(self.road.width/10 + self.car_size[1]),self.road.width*2,3))
+        np.random.seed(seed)
 
-        pos = [self.road.l1c, int(self.road.length/2 + 2*self.car_size[1])]
-        self.ov.append(car(pos, self.car_size, self.road.l1v))
-        pos = [self.road.l1c, int(self.road.length/2 - 3*self.car_size[1])]
-        self.ov.append(car(pos, self.car_size, self.road.l1v))
+        pos1, pos2 = -2000, -2000
+        car_dists = np.arange(int(self.road.width/10 + self.car_size[1]),self.road.width*7,7)
+        for i in range(120):
+            self.ov.append(car([self.road.l2c, pos1], self.car_size,  self.road.l2v))
+            if not int(self.road.length/2 - 3*self.car_size[1]) < pos2 < int(self.road.length/2 + 3*self.car_size[1]):
+                self.ov.append(car([self.road.l1c, pos2], self.car_size,  self.road.l1v))
+            pos1 += np.random.choice(car_dists)
+            pos2 += np.random.choice(car_dists)
+        # print(pos1,pos2)
 
         pos = [self.road.l1c, int(self.road.length/2)]
         self.car = car(pos, self.car_size, self.road.l1v)
         self.camera = [self.car.pos[1]]
         self.render()
 
-    def draw_dist(self):
-        if hasattr(self, 'dist'):
-            try:
-                pygame.draw.rect(self.DISPLAY, THECOLORS.get('blue'), (ROAD_W + 20, 20+50*(1-self.dist[0]), 30, 10+50*self.dist[0]))
-                pygame.draw.rect(self.DISPLAY, THECOLORS.get('blue'), (ROAD_W + 60, 20+50*(1-self.dist[1]), 30, 10+50*self.dist[1]))
-                pygame.draw.rect(self.DISPLAY, THECOLORS.get('blue'), (ROAD_W + 100, 20+50*(1-self.dist[2]), 30,10+50*self.dist[2]))
-
-                myfont = pygame.font.SysFont("helvetica", 10)
-
-                # render text
-                label1 = myfont.render("left", 1, (0, 0, 0))
-                label2 = myfont.render("nothing", 1, (0, 0, 0))
-                label3 = myfont.render("right", 1, (0, 0, 0))
-                self.DISPLAY.blit(label1, (ROAD_W + 27, 85))
-                self.DISPLAY.blit(label2, (ROAD_W + 60, 85))
-                self.DISPLAY.blit(label3, (ROAD_W + 106, 85))
-
-            except TypeError:
-                print((ROAD_W + 30, 20+100*(1-self.dist[0]), 30, 20+100*self.dist[0]))
-                print((ROAD_W + 60, 20+100*(1-self.dist[1]), 30, 20+100*self.dist[1]))
-                print((ROAD_W + 100, 20+100*(1-self.dist[2]), 30, 20+100*self.dist[2]))
-
     def render(self):
-        self.road.pole = np.zeros_like(self.road.pole)
-        w = int(self.car_size[0])
-        h = int(self.car_size[1])
-        for v in self.ov:
-            x,y = v.coord(self.car.pos[1])
-            self.road.pole[y:y+h, x:x+w] = 1.0
-        x, y = self.car.coord(self.car.pos[1])
-        self.road.pole[y:y + h, x:x + w] = 1.0
 
         self.DISPLAY.fill(THECOLORS.get('white'))
-        pygame.draw.rect(self.DISPLAY, THECOLORS.get('grey'), (0, 0, ROAD_W, ROAD_H))
+        pygame.draw.rect(self.DISPLAY, THECOLORS.get('grey'), (0, 0, ROAD_W*5, ROAD_H*5))
 
         for v in self.ov:
             v.draw(self.DISPLAY, self.car.pos[1])
         self.car.draw(self.DISPLAY, self.car.pos[1])
 
-        self.draw_dist()
-
         pygame.display.update()
+
+        bluepx = pygame.surfarray.pixels_blue(self.DISPLAY)
+        bluepx = np.array(bluepx)
+        bluepx[bluepx == 190] = 0
+        bluepx[bluepx == 255] = 1
+        self.road.pole = bluepx[::5, ::5]
 
         quit_ = False
         for event in pygame.event.get():
@@ -237,25 +204,25 @@ class Road_game:
         return quit_
 
 
-    # p_a represents a probability to be in the 2nd state
-    def move(self, p_a):
+    def move(self, d_v):
 
-        self.dist = p_a.copy()
-        a = np.random.choice(self.actions, p=p_a)
+        self.dist = d_v.copy()
 
-        if (self.car.pos[0] > self.road.l2c and a == -1) or (self.car.pos[0] <= self.road.l1c and a == 1):
-            self.car.pos[0] += 3*a
+        if (self.car.pos[0] > self.road.l2c and d_v[0] == -1) or (self.car.pos[0] < self.road.l1c and d_v[0] == 1):
+            self.car.pos[0] += 2*d_v[0]
 
-        self.car.v = self.road.vel[clamp(self.road.l1c - self.car.pos[0], 0, len(self.road.vel)-1)]
+        # self.car.v = self.road.vel[clamp(self.road.l1c - self.car.pos[0], 0, len(self.road.vel)-1)]
+        if 0 < self.car.v < 16:
+            self.car.v += d_v[1]
 
         self.update_game()
 
         if self.collision():
-            self.game_over=True
-            return -3
+            self.game_over = True
+            return -300
         if self.goal_reached():
-            self.game_over=True
-            return 3
+            self.game_over = True
+            return 30
         return -0.01
 
     def update_game(self):
@@ -264,68 +231,22 @@ class Road_game:
         self.car.pos[1] += self.car.v
         self.render()
 
+    def quit(self):
+        pygame.quit()
+
+def manual_control():
+
+    game = Road_game()  # instance of a game
+
+    while True:
+        total_rew = game.play_one()
+        print(total_rew)
+
+def replay_game(traj):
+    game = Road_game()
+    game.replay(traj)
+
+
 if __name__ == '__main__':
-    # TODO: make beta_step and alpha_step fulfil condition from paper
-    N_games_learn = 2000 # number of games to play for learning
-    N_games_test = 200 # number of games to play for data gathering
-    length_of_game = 40  # number of steps in one game
-    theta_update_step = 75
-
-    variance_bounds = [4.0,3.0,2.0,0.0]  # variance bounds
-
-    tr_plot, theta_plot, Var_plot, G_plot = [], [], [], []  # data for plots
-
-    for v in variance_bounds:
-        game = Road_game(v)  # instance of a game
-        total_rews, theta, Var, G = [], [], [], []
-        for i in range(N_games_learn):
-            ut = False  # parameter which specifies if theta is updated in that iteration
-            if i == int(2*N_games_learn/3):  # in the middle of the game make lambda to be almost 1.0
-                # this is related to the approximation of COP solution approximation
-                # equations (9) and (10) and 7 lines of text under
-                game.lam = 0.99
-            if i % theta_update_step == 0 and i != 0:
-                ut = True  # theta gets updated every 20. iteration
-                # theta.append(game.theta.copy())  # gathering data for graph
-            Var.append(game.Var)  # gathering data for graph
-            G.append(game.G)  # gathering data for graph
-            total_rew, zk = game.play_one(length_of_game)
-            # print('Total reward, iteration:', total_rew, i)
-            game.update(total_rew, zk, update_theta=ut)  # finally update everything
-
-        for i in range(N_games_test):
-            total_rew, _ = game.play_one(length_of_game)  # gathering data for graph without update
-            total_rews.append(total_rew)  # gathering data for graph
-
-        tr_plot.append(total_rews)  # gathering data for graph
-        # theta_plot.append(theta)  # gathering data for graph
-        Var_plot.append(Var)  # gathering data for graph
-        G_plot.append(G)  # gathering data for graph
-
-    plt.figure()
-    for rew, v in zip(tr_plot,variance_bounds):
-        plt.hist(rew, label='Var bound %f'%v)
-    plt.legend()
-    plt.title('Total rewards')
-
-    # plt.figure()
-    # for theta, v in zip(theta_plot,variance_bounds):
-    #     plt.plot(np.arange(N_games_learn / theta_update_step - 1), np.array(theta)[:, 0],
-    #              np.arange(N_games_learn / theta_update_step - 1), np.array(theta)[:, 1],
-    #              np.arange(N_games_learn / theta_update_step - 1), np.array(theta)[:, 2],
-    #              label='Var bound %f'%v)
-    # plt.legend()
-    # plt.title('Patameters theta')
-
-    plt.figure()
-    for rew, v in zip(Var_plot,variance_bounds):
-        plot_run_avg(rew, 100, label='Var bound %f'%v)
-    plt.legend()
-    plt.title('Variance')
-
-    plt.figure()
-    for rew, v in zip(G_plot,variance_bounds):
-        plot_run_avg(rew, 200, label='Var bound %f'%v)
-    plt.legend()
-    plt.title('Mean reward')
-    plt.show()
+    # replay_game(np.load('trajectories/traj_1527959902.npy'))
+    manual_control()
---
+++ supervised_agent.py
@@ -0,0 +1,166 @@
+import numpy as np
+import tensorflow as tf
+
+import tensorflow as tf
+import logging
+import os
+import matplotlib
+matplotlib.use("TkAgg")
+# from matplotlib import pyplot as plt
+import numpy as np
+import sklearn
+
+from simple_car_game import *
+from pdb import set_trace
+
+
+GAMMA = 0.9
+
+def softmax(x):
+    """Compute softmax values for each sets of scores in x."""
+    e_x = np.exp(x - np.max(x))
+    return e_x / e_x.sum(axis=0) # only difference
+
+class SGDRegressor_occupancy:
+    def __init__(self, xd, restore=True):
+
+        self.save_path = 'graphs/graph_supervised/graph.ckpt'
+
+        self.states = tf.placeholder(tf.float32, shape=[None,xd-2], name='states')
+        self.actions = tf.placeholder(tf.float32, shape=[None,9], name='actions')
+        self.action_ind = tf.placeholder(tf.int32, shape=[None, 2], name='act_inds')
+        self.weight_ind = tf.placeholder(tf.int32, shape=[None, 2], name='weiht_inds')
+
+        with tf.variable_scope('policy'):
+            pi0 = tf.layers.dense(self.states, 64, activation=tf.nn.sigmoid, use_bias=True)
+            pi1 = tf.layers.dense(pi0, 32, activation=tf.nn.sigmoid, use_bias=True)
+            pi2 = tf.layers.dense(pi1, 16, activation=tf.nn.sigmoid, use_bias=True)
+            pi = tf.layers.dense(pi2, 9, activation=tf.nn.softmax, use_bias=True)
+            self.pi = tf.gather_nd(pi, self.action_ind)
+
+        self.weights = tf.constant(np.array([[1,1,1,1,0.1,1,1,1,1]]).astype('float32'))
+        self.weights_picked = tf.gather_nd(self.weights, self.weight_ind)
+
+        self.smce = tf.losses.mean_squared_error(tf.gather_nd(self.actions, self.action_ind), self.pi, weights=self.weights_picked)
+
+        self.cost = self.smce
+
+        # ops we want to call later
+        optimizer = tf.train.AdamOptimizer(learning_rate=0.001)
+        self.train = optimizer.minimize(self.cost)
+
+        # start the session and initialize params
+        init = tf.global_variables_initializer()
+        self.saver = tf.train.Saver()
+        self.session = tf.Session()
+        if restore:
+            self.saver.restore(self.session, self.save_path)
+        else:
+            self.session.run(init)
+
+        # self.session.graph.finalize()
+
+
+    def partial_fit(self, sa_pairs, actions):
+        action_inds = [game.actios.index(s[-2:].tolist()) for s in sa_pairs]
+        action_inds = np.vstack((np.arange(0, len(sa_pairs), 1), action_inds))
+        inds = action_inds.T
+        weight_inds = [game.actios.index(s[-2:].tolist()) for s in sa_pairs]
+        weight_inds = np.vstack((np.zeros((1, len(sa_pairs))), weight_inds))
+        weight_inds = weight_inds.T
+        self.session.run(self.train, feed_dict={self.actions:actions, self.states:sa_pairs[:,:-2],self.action_ind:inds, self.weight_ind:weight_inds})
+
+    def predict(self, sa_pairs):
+        action_inds = [game.actios.index(s[-2:].tolist()) for s in sa_pairs]
+        action_inds = np.vstack((np.arange(0, len(sa_pairs), 1), action_inds))
+        inds = action_inds.T
+        weight_inds = [game.actios.index(s[-2:].tolist()) for s in sa_pairs]
+        weight_inds = np.vstack((np.zeros((1, len(sa_pairs))), weight_inds))
+        weight_inds = weight_inds.T
+        return self.session.run(self.pi, feed_dict={self.states:sa_pairs[:,:-2], self.action_ind:inds, self.weight_ind:weight_inds})
+
+    def save_sess(self):
+        self.saver.save(self.session, self.save_path)
+
+    def look_incide(self,sa_pairs, actions):
+        action_inds = [game.actios.index(s[-2:].tolist()) for s in sa_pairs]
+        action_inds = np.vstack((np.arange(0, len(sa_pairs), 1), action_inds))
+        inds = action_inds.T
+
+        weight_inds = [game.actios.index(s[-2:].tolist()) for s in sa_pairs]
+        weight_inds = np.vstack((np.zeros((1,len(sa_pairs))), weight_inds))
+        weight_inds = weight_inds.T
+
+        pi = self.session.run(self.pi, feed_dict={self.states: sa_pairs[:,:-2],self.action_ind:inds})
+        weights =  self.session.run(self.weights_picked, feed_dict={self.states: sa_pairs[:,:-2],self.weight_ind:weight_inds})
+        softm = self.session.run(self.smce, feed_dict={self.actions:actions, self.states:sa_pairs[:,:-2],self.action_ind:inds,self.weight_ind:weight_inds})
+        c0 = self.session.run(self.cost, feed_dict={self.actions:actions, self.states:sa_pairs[:,:-2], self.action_ind:inds,self.weight_ind:weight_inds})
+        comy = np.multiply(weights,softm)
+        comy_mean = np.mean(comy)
+
+
+def sample_trajectories(game, model):
+    trajectories=[]
+
+    while len(trajectories)<20:
+        game.init_game(seed=None)
+        # s_a_pairs = []
+        total_rew = 0
+        while not game.game_over:
+            st = game.state
+
+            # print(np.repeat(st,len(game.actios),axis=0))
+            # print(game.actios)
+
+            action_probs = model.predict(np.hstack((np.repeat([st],len(game.actios),axis=0), game.actios)))
+            idx = np.random.choice(range(9), p=action_probs.ravel())
+            d_v = game.actios[idx]
+            print(np.sum(action_probs), d_v)
+            # s_a_pairs.append(np.hstack((st, d_v)))
+            rew = game.move(d_v)
+            total_rew += rew
+            time.sleep(0.1)
+            # print('\rVelocity: {}'.format(game.car.v), end='')
+        # trajectories.append(s_a_pairs)
+    # return trajectories
+
+def one_hot(actions, game):
+    new_y = np.zeros((len(actions), 9))
+    for i, a in enumerate(actions):
+        idx = game.actios.index(a.tolist())
+        new_y[i,idx] = 1.0
+
+    return new_y
+
+if __name__ == '__main__':
+    expert_traj = []
+
+    traj_path = 'trajectories_all/trajectories_rand_big/'
+
+    game = Road_game()
+
+    for i, t in enumerate(os.listdir(traj_path)):
+        raw_traj = np.load(traj_path + t)
+        expert_traj.append(raw_traj)
+
+    expert_traj = np.array(expert_traj)
+    expert_tuples = np.vstack(expert_traj)
+    X = expert_tuples[:, :-2]
+    y = expert_tuples[:, -2:]
+    y = one_hot(y, game)
+    model = SGDRegressor_occupancy(expert_traj[0].shape[1])
+
+    logging.basicConfig(filename='images/road_game.log', level=logging.DEBUG)
+
+    N_iterations = 2000
+
+    for i in range(N_iterations):
+        print('{}/{}'.format(i, N_iterations))
+
+        logging.debug('======Iteration #{}======'.format(i))
+        model.partial_fit(expert_tuples, y)
+        # model.look_incide(expert_tuples, y)
+
+    model.save_sess()
+
+    sample_trajectories(game, model)
